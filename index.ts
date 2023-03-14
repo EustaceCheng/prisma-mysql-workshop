aea69ffd6d05a296229ec3f41a09cd04892c0c02
parker cheng <fbi0258zzz@gmail.com>
2023-03-14 11:33:23 +0800

    prettier

--- index.ts
+++ index.ts
@@ -1,18 +1,15 @@
-import { PrismaClient, Prisma } from "@prisma/client";
-import express, { Request, Response } from "express";
-import category from "./router/category";
-import task from "./router/task";
-import user from "./router/user";
+import express from 'express';
+import category from './router/category';
+import task from './router/task';
+import user from './router/user';
 
 const app = express();
 app.use(express.json());
-const prisma = new PrismaClient();
 
-app.use("/category", category);
-app.use("/task", task);
-
-app.use("/user", user);
+app.use('/category', category);
+app.use('/task', task);
+app.use('/user', user);
 
 app.listen(3001, () => {
-  console.log("SERVER RUNNING ON PORT 3001");
+    console.log('SERVER RUNNING ON PORT 3001');
 });
---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    trailingComma: 'all',
+    singleQuote: true,
+    tabWidth: 4,
+    printWidth: 100,
+    arrowParens: 'avoid',
+    semi: true,
+};
